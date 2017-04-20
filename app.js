8d49cdda1946c7d1bb3fd499e2ef0787f73a4bb8
Ryan McCombe <ryanmccombe@gmail.com>
2017-04-20 01:36:51 +0100

    Express boilerplate

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(3050, () => {
+    console.log('listening on 3050');
+});
