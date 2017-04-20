539c26815bb88c74fb498a10c725dafede73fab5
Ryan McCombe <ryanmccombe@gmail.com>
2017-04-20 06:26:43 +0100

    Error catching middleware

--- app.js
+++ app.js
@@ -13,4 +13,8 @@ const app = express();
 app.use(bodyParser.json());
 routes(app);
 
+app.use((err, req, res, next) => {
+    err && res.status(422).send({ error: err.message });
+});
+
 module.exports = app;
--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -4,10 +4,11 @@ module.exports = {
     greeting(req, res) {
         res.send({ hi: 'there' });
     },
-    create(req, res) {
+    create(req, res, next) {
         const driverProps = req.body;
 
         Driver.create(driverProps)
-            .then(driver => res.send(driver));
+            .then(driver => res.send(driver))
+            .catch(next);
     }
 };
